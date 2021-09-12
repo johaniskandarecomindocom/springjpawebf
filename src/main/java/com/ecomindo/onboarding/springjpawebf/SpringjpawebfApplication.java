9fbe2bb9d8fb4638bd0e6d108b11c1d5557841bd
Johan Iskandar <johan.iskandar@ecomindo.com>
2021-09-12 21:56:21 +0700

    First Commit

---
+++ src/main/java/com/ecomindo/onboarding/springjpawebf/SpringjpawebfApplication.java
@@ -0,0 +1,13 @@
+package com.ecomindo.onboarding.springjpawebf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpawebfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpawebfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomindo/onboarding/springjpawebf/SpringjpawebfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomindo.onboarding.springjpawebf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpawebfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
